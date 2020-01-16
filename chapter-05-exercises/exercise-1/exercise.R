45e999ef20968d3b52afe6aa6277227a93643ddd
Yizhe Zhang <yizneh@uw,edu>
2020-01-16 15:49:34 -0800

    exercise week 2 added

--- chapter-05-exercises/exercise-1/exercise.R
+++ chapter-05-exercises/exercise-1/exercise.R
@@ -1,26 +1,27 @@
 # Exercise 1: practice with basic R syntax
 
 # Create a variable `hometown` that stores the city in which you were born
-
+hometown <- "Qingdao"
 
 # Assign your name to the variable `my_name`
-
+my_name <- "Yizhe Zhang"
 
 # Assign your height (in inches) to a variable `my_height`
-
-
+my_height <- "5'7"
+  
 # Create a variable `puppies` equal to the number of puppies you'd like to have
-
+puppies <- 2
 
 # Create a variable `puppy_price`, which is how much you think a puppy costs
-
+puppy_price <- 1000
 
 # Create a variable `total_cost` that has the total cost of all of your puppies
-
+total_cost <- puppy_price*puppies
 
 # Create a boolean variable `too_expensive`, set to TRUE if the cost is greater 
 # than $1,000
-
+too_expensive <- total_cost > 1000
 
 # Create a variable `max_puppies`, which is the number of puppies you can 
 # afford for $1,000
+max_puppies<- 1000/puppy_price
--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -2,17 +2,25 @@
 
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
+add_three <- function(number) {
+result <-sum(number,3)
+return(result)
+}
 
 
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
 # function
+add_three(7)
 
 
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
 # of feet and a number of inches
 # The function should return the equivalent length in meters
-
+imperial_to_metric <- function(feet,inches) { 
+  length <- feet*0.3+inches*0.025
+  return(length)
+}
 
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
-
+imperial_to_metric(5,8)
