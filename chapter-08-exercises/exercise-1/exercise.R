cf2d987b9a71be2b4520462025a3a1d94b45c6f1
Yizhe Zhang <yizneh@uw,edu>
2020-01-24 00:00:29 -0800

    exercise week three

--- chapter-08-exercises/exercise-1/exercise.R
+++ chapter-08-exercises/exercise-1/exercise.R
@@ -1,28 +1,32 @@
 # Exercise 1: creating and accessing lists
 
 # Create a vector `my_breakfast` of everything you ate for breakfast
-
+my_breakfast<-list("milk","bread")
 
 # Create a vector `my_lunch` of everything you ate (or will eat) for lunch
-
+my_lunch <-list("fries","chicken tenders")
 
 # Create a list `meals` that has contains your breakfast and lunch
+meal<-list(breakfast=my_breakfast,lunch=my_lunch)
 
 
 # Add a "dinner" element to your `meals` list that has what you plan to eat 
 # for dinner
-
+meal$dinner <-c("ramen","egg")
+print(dinner)
 
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
-
+dinner<-meal$dinner
+print(dinner)
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason beyond practice)
-
-
+meal[[5]]<-meal[["lunch"]]
+print(meal)
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early_meals`
+early_meals <-meal[c("breakfas","lunch")]
 
 
 
@@ -30,11 +34,15 @@
 
 # Create a list that has the number of items you ate for each meal
 # Hint: use the `lappy()` function to apply the `length()` function to each item
-
-
+length(meal$breakfast)
+lapply(meal,length)
 # Write a function `add_pizza` that adds pizza to a given meal vector, and
 # returns the pizza-fied vector
-
+add_pizza<-function(meal_vector){
+  new_vector<-c(meal_vector,"pizza")
+  return(new_vector)
+}
+add_pizza(c("nachos"))
 
 # Create a vector `better_meals` that is all your meals, but with pizza!
-
+lapply(meal,add_pizza)
--- chapter-10-exercises/exercise-2/exercise.R
+++ chapter-10-exercises/exercise-2/exercise.R
@@ -3,45 +3,50 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
-
-
+employees <- paste("employees", 1:100)
+print(employees)
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
-
-
+salary_2017<-runif(100,40000,50000)
+print(salary_2017)
 # Create a vector of 100 annual salary adjustments between -5000 and 10000.
 # (A negative number represents a salary decrease due to corporate greed)
 # Again use the `runif()` function to pick 100 random numbers in that range.
-
+adjustment<- runif(100,-5000,10000)
 
 # Create a data frame `salaries` by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
-
+salaries<-data.frame(employees,salary_2017,adjustment)
+print(salaries)
 # Add a column to the `salaries` data frame that represents each person's
 # salary in 2018 (e.g., with the salary adjustment added in).
-
-
+salary_2018<-salary_2017+adjustment
+salaries<-data.frame(salaries,salary_2018)
+print(salaries)
 # Add a column to the `salaries` data frame that has a value of `TRUE` if the 
 # person got a raise (their salary went up)
-
-
+salaries$raise<-salaries$adjustment>0
+print(salaries)
 
 ### Retrieve values from your data frame to answer the following questions
 ### Note that you should get the value as specific as possible (e.g., a single
 ### cell rather than the whole row!)
 
 # What was the 2018 salary of Employee 57
-
-
+salaries[57,"salary_2018"]
+print(salaries[salaries$employees=="employees 57","salary_2018"])
 # How many employees got a raise?
 
+nrow(salaries[salaries$raise==TRUE,])
 
 # What was the dollar value of the highest raise?
 
+print(biggest_raise <-max(salaries$adjustment))
+
+salaries[salaries$adjustment==biggest_raise,"adjustment"]
 
 # What was the "name" of the employee who received the highest raise?
-
+salaries[salaries$adjustment==biggest_raise,"employees"]
 
 # What was the largest decrease in salaries between the two years?
 
@@ -53,10 +58,13 @@
 
 
 # For people who did not get a raise, how much money did they lose on average?
-
-
+paycut<-salaries[salaries$raise == FALSE,]
+mean(paycut$adjustment)
+mean(salaries[salaries$raise == FALSE,"adjustment"])
 ## Consider: do the above averages match what you expected them to be based on 
 ## how you generated the salaries?
 
 # Write a .csv file of your salary data to your working directory
-
+people_csv<-read.csv("example.csv",stringsAsFactors = FALSE)
+getwd()
+setwd(~/Users/Lenovo/Documents/info201/book-exercises/chapter-10-exercises/exercise-2)
