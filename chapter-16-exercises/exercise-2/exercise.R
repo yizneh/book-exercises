89cce7971338741815b02f83ccd669ae805df28b
Yizhe Zhang <yizneh@uw,edu>
2020-02-14 15:57:04 -0800

    exercise week 6

--- chapter-16-exercises/exercise-2/exercise.R
+++ chapter-16-exercises/exercise-2/exercise.R
@@ -12,47 +12,65 @@ library("ggplot2")
 
 # Draw a column (bar) chart of diamonds cuts by price, with each bar filled by 
 # clarity. You should see a _stacked_ bar chart.
-
+ggplot(data=diamonds)+
+  geom_col(mapping = aes(x=cut,y=price,fill=clarity))
 
 # Draw the same chart again, but with each element positioned to "fill" the y axis
-
+ggplot(data = diamonds) +
+  geom_col(mapping = aes(x = cut, y = price, fill = clarity), position = "fill")
 
 # Draw the same chart again, but with each element positioned to "dodge" each other
+ggplot(data = diamonds) +
+  geom_col(mapping = aes(x = cut, y = price, fill = clarity), position = "dodge")
 
 
 # Draw a plot with point geometry with the x-position mapped to `cut` and the 
 # y-position mapped to `clarity`
 # This creates a "grid" grouping the points
-
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = cut, y = clarity))
 
 # Use the "jitter" position adjustment to keep the points from all overlapping!
 # (This works a little better with a sample of diamond data, such as from the 
 # previous exercise).
-
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = cut, y = clarity),position = "jitter")
 
 
 ## Scales
 
 # Draw a "boxplot" (with `geom_boxplot`) for the diamond's price (y) by color (x)
-
+ggplot(data=diamonds_sample)+
+  geom_boxplot(mapping = aes(x=color,y=price))+
+  scale_y_log10()
 
 # This has a lot of outliers, making it harder to read. To fix this, draw the 
 # same plot but with a _logarithmic_ scale for the y axis.
-
+ggplot(data=diamonds_sample)+
+  geom_violin(mapping = aes(x=color,y=price))+
+  
 
 # For another version, draw the same plot but with `violin` geometry instead of 
 # `boxplot` geometry!
 # How does the logarithmic scale change the data presentation?
-
+  ggplot(data=diamonds_sample)+
+  geom_violin(mapping = aes(x=color,y=price))+
+  scale_y_log10()
 
 # Another interesting plot: draw a plot of the diamonds price (y) by carat (x), 
 # using a heatmap of 2d bins (geom_bin2d)
 # What happens when you make the x and y channels scale logarithmically?
-
+ggplot(data=diamonds_sample)+
+  geom_bin2d(mapping = aes(x=carat,y=price))+
+  scale_y_log10()+
+  scale_x_log10()
 
 # Draw a scatter plot for the diamonds price (y) by carat (x). Color each point
 # by the clarity (Remember, this will take a while. Use a sample of the diamonds 
 # for faster results)
+ggplot(data=diamonds_sample)+
+  geom_point(mapping = aes(x=carat,y=price,color=clarity))+
+  scale_color_brewer(palette="Purples",direction = 1)
 
 
 # Change the color of the previous plot using a ColorBrewer scale of your choice. 
@@ -79,7 +97,9 @@ library("ggplot2")
 
 # Take the scatter plot of price by carat data (colored by clarity) and add 
 # _facets_ based on the diamond's `color`
-
+ggplot(data=diamonds)+
+  geom_point(mapping = aes(x=carat,y=price,color=color))+
+  facet_wrap(~clarity)
 
 
 ## Saving Plots
--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -7,10 +7,12 @@ library("jsonlite")
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source("api_key.R") 
+# go to that file, selet all, and hit ctrl enter 
+query_params <- list("api_key" = nyt_key)
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+movie_name <- "Parasite"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -20,14 +22,19 @@ library("jsonlite")
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+endpoint<- "reviews/search.json"
+uri<- paste0(base_uri,endpoint)
 
+query_parame_list <- list("api_key" = nyt_key, "movie name"=movie_name)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
-
+response <- GET(uri,query = query_parame_list)
+print(response)
 # What kind of data structure did this produce? A data frame? A list?
-
+body<- content(response,"text",encoding = "UTF-8")
+data <- fromJSON(body)
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
